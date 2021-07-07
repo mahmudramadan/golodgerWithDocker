109768199aa21d785d928c4d4dbb238e7391f0c3
Mahmoud Ramadan <37539803+mahmudramadan@users.noreply.github.com>
2021-07-07 12:50:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $adverisorsApiArray = ['https://f704cb9e-bf27-440c-a927-4c8e57e3bad1.mock.pstmn.
 
 if (count($adverisorsApiArray) > 0) {
     $AdvertisorsData = new AdvertisorsData($adverisorsApiArray);
-    $AdvertisorsData->getAdvertisorsRoomsJson();
-} else {
-    echo json_encode(array('status' => 200, 'foundRooms' => false, 'rooms' => []));
-}
+    return $AdvertisorsData->getAdvertisorsRoomsJson();
+} 
+ return json_encode(array('status' => 200, 'foundRooms' => false, 'rooms' => []));
